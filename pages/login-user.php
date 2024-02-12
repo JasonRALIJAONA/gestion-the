ee39dee2ed5dcafa0821d31d10c58a36ecfc0551
nathanJuanito <154326723+nathanJuanito@users.noreply.github.com>
2024-02-12 14:45:19 +0300

    login

---
+++ pages/login-user.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THE</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.css">
+</head>
+<body style="background-color:#e7e7e7;background: url(../assets/img/fondLogin.jpg);background-repeat: no-repeat;background-size">
+<center>
+        
+        <div class="row" style="margin-top: 200px;width:500px ;padding-left: 40px;height: 350px;border-radius: 10px;background-color: white;box-shadow:0 5px 10px rgba(0, 0, 0, 0.05);padding-right: 30px;">
+            <h1>LOGIN</h1>
+            <form class="form-horizontal" action="" method="post" style="margin-top: 50px; ">
+                <div class="form-group">
+                  <label class="col-sm-2 control-label col-lg-4">E-Mail</label>
+                  <div class="col-sm-10 col-lg-6">
+                   <input type="text" name="" placeholder="E-Mail" class="form-control">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="col-sm-2 control-label col-lg-4">Mot de passe</label>
+                  <div class="col-sm-10 col-lg-4">
+                   <input type="password" name="" placeholder="Mot de passe" class="form-control">
+                  </div>
+                </div>
+                <p style="color:red">Veuillez réessayer</p>
+                <p style="margin-top: 20px;"><a href="">Se connecter en tant qu'administrateur?</a></p>
+                
+
+
+                
+                <div class="form-group">
+                    <button style="
+                    margin-top: 20px;" type="submit" class="btn btn-primary">Se connecter</button>
+                </div>
+              </form>
+              
+            </div>
+        
+    </center>
+    
+</body>
+</html>
