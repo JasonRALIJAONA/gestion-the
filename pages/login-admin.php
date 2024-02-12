135ced95f5be3d0c56b2bac3486473fc5942d502
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-02-12 03:19:50 -0900

    login-moitie

--- pages/login-admin.php
+++ pages/login-admin.php
@@ -11,17 +11,17 @@
         
         <div class="row" style="margin-top: 200px;width:500px ;padding-left: 40px;height: 350px;border-radius: 10px;background-color: white;box-shadow:0 5px 10px rgba(0, 0, 0, 0.05);padding-right: 30px;">
             <h1>LOGIN(Admin)</h1>
-            <form class="form-horizontal" action="" method="post" style="margin-top: 50px; ">
+            <form class="form-horizontal" action="../traitement/traite-login-admin" method="post" style="margin-top: 50px; ">
                 <div class="form-group">
                   <label class="col-sm-2 control-label col-lg-4">E-Mail</label>
                   <div class="col-sm-10 col-lg-6">
-                   <input type="text" name="" placeholder="E-Mail" class="form-control">
+                   <input type="text" name="email" placeholder="E-Mail" class="form-control">
                   </div>
                 </div>
                 <div class="form-group">
                   <label class="col-sm-2 control-label col-lg-4">Mot de passe</label>
                   <div class="col-sm-10 col-lg-4">
-                   <input type="password" name="" placeholder="Mot de passe" class="form-control">
+                   <input type="password" name="mdp" placeholder="Mot de passe" class="form-control">
                   </div>
                 </div>
                 <p style="color:red">Veuillez réessayer</p>
