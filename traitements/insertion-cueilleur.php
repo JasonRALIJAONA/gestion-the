e26f03afdbd02c55a9d8ca637ed3739fb6bfdc90
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-02-12 11:02:26 -0900

    suite-cuielleur

--- traitements/insertion-cueilleur.php
+++ traitements/insertion-cueilleur.php
@@ -2,6 +2,6 @@
 include_once '../inc/fonction.php';
 
 insertCueilleur($_POST['nom'], $_POST['genre'], $_POST['dateNaissance']);
-header('Location:../pages/gestion-cueilleurs.php');
+header('Location:../pages/gestion-cueilleurs.html');
 
 ?>
