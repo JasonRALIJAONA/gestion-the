fa6bcd0f6bacb5f51ef3694a56754b27b18cc78a
JasonRALIJAONA <jasonralijaona@gmail.com>
2024-02-13 05:40:26 -0900

    premier ajax prevision

--- js/prevision.js
+++ js/prevision.js
@@ -19,22 +19,22 @@ function connexion() {
               // Accédez à l'élément form …
 
             //  Définissez ce qui se passe si la soumission s'est opérée avec succès
-            xhr.addEventListener("load", function(event) {
-            $msg=(event.target.responseText!="")?"Cueillette effectue ":"OK";
-            alert($msg);
-            });
+        //     xhr.addEventListener("load", function(event) {
+        //     $msg=(event.target.responseText!="")?"Cueillette effectue ":"OK";
+        //     alert($msg);
+        //     });
       
-          // Definissez ce qui se passe en cas d'erreur
-            xhr.addEventListener("error", function(event) {
-            alert('Oups! Quelque chose s\'est mal passé.');
-            });
+        //   // Definissez ce qui se passe en cas d'erreur
+        //     xhr.addEventListener("error", function(event) {
+        //     alert('Oups! Quelque chose s\'est mal passé.');
+        //     });
       
           xhr.onreadystatechange  = function() 
           { 
              if(xhr.readyState  == 4){
               if(xhr.status  == 200) {
                 var retour = JSON.parse(xhr.responseText);
-                console.log(retour);
+                displayPrevision(retour);
               } else {
                   document.dyn="Error code " + xhr.status;
               }
@@ -43,7 +43,7 @@ function connexion() {
           }; 
 
           // Configurez la requête
-          xhr.open("POST", "../traitements/traite-cueillete.php");
+          xhr.open("POST", "../traitements/get-liste-parcelle.php");
       
           // Les données envoyées sont ce que l'utilisateur a mis dans le formulaire
           xhr.send(formData);
@@ -61,4 +61,62 @@ function connexion() {
         });
       });
       
+}
+
+function displayPrevision(tab) {
+    var resultat=document.getElementsByTagName("row")[0];
+    if (resultat.hasChildNodes()) {
+        while (resultat.firstChild) {
+          resultat.removeChild(resultat.firstChild);
+        }  
+    }
+
+    tab.forEach(element => {
+        var tempDiv=document.createElement("div");
+        tempDiv.class="col-sm-6 col-md-4";
+
+        var thumbnail=document.createElement("div");
+        thumbnail.className="thumbnail parcelles";
+        thumbnail.style="padding-left: 20px;padding-right: 20px";
+
+        var nomParcelle=document.createElement("p");
+        nomParcelle.innerHTML=element['numero'];
+
+        thumbnail.appendChild(nomParcelle);
+
+        var centre=document.createElement("center");
+        var h3=document.createElement("h3");
+
+        centre.appendChild(h3);
+
+        thumbnail.appendChild(centre);
+
+        var surface=document.createElement("p");
+        surface.innerHTML=element['surface'];
+
+        thumbnail.appendChild(surface);
+
+        var image=document.createElement("img");
+        image.source="../assets/img/the.jpg";
+
+        thumbnail.appendChild(image);
+
+        var caption=document.createElement("div");
+        caption.className="caption";
+
+        var pieds=document.createElement("p");
+        pieds.innerHTML="Nombre de pieds : "+element['nbPied'];
+
+        var restant=document.createElement("p");
+        restant.innerHTML="Poids The restant : "+element['poids'];
+
+        caption.appendChild(pieds);
+        caption.appendChild(restant);
+
+        thumbnail.appendChild(caption);
+
+        tempDiv.appendChild(thumbnail);
+        resultat.appendChild(tempDiv)
+    });
+
 }
